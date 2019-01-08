e63f9fff8ee91f59d8a010b6b807374127d6fb18
taralesca <ovidiu.taralesca@gmail.com>
2019-01-09 00:27:33 +0200

    test

--- src/write.c
+++ src/write.c
@@ -1,33 +1,25 @@
 #include "../lib/write.h"
 
-int sifs_write(
-    const char* path,
-    const char *buf,
-    size_t size,
-    off_t offset,
-    struct fuse_file_info* fi
-) {
+int sifs_write(const char* path, const char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
 	//logger(ERROR, "UNIMPLEMENTED: write, path: %s\n", path);
 	struct fuse_context* context;
 	context=fuse_get_context();
-	
 	
 	struct node*root;
 	root=(struct node*)context->private_data;
 	struct node*n;
 	n=get_node_from_path(root,path);
-	
-	
-	size_t file_length = strlen(buf);
-	if (offset < file_length) {
-		if (offset + size > file_length)
-			size = file_length-offset;
-		logger(DEBUG, "[wrote] %s\n\n\n", buf);
-		memcpy(buf + offset, buf, size);
-	} else {
-		return -1;
-	}
-
+	n->file = realloc(n->file, strlen(buf));
+	//printf("\n\n\n\n\n\n\n\n\n\n\n\n%s", buf);
+	//~ size_t file_length = strlen(buf);
+	//~ if (offset < file_length) {
+		//~ if (offset + size > file_length)
+			//~ size = file_length-offset;
+		//~ logger(DEBUG, "[wrote] %s\n\n\n", buf);
+		memcpy(n->file + offset, buf, size);
+	//~ } else {
+		//~ return -1;
+	//~ }
   return size;
 }
 
