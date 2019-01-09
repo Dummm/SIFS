710d8a79b9a0b8edbf00561e8d659370f332ec6d
taralesca <ovidiu.taralesca@gmail.com>
2019-01-09 23:11:53 +0200

    Destroy kinda works

--- src/destroy.c
+++ src/destroy.c
@@ -3,11 +3,13 @@ int fd; /* gets value from sifs.c */
 int fdd;
 int enfd;
 void print_tree2(struct node *n) {
-	logger(DEBUG, "\t%s\n", n->header->name);
+	//~ logger(DEBUG, "\t%s%d\n", n->header->name, strtoul(n->header->size, NULL, 8));
 	if(strcmp(n->header->name, "./") != 0){
 		write(fdd, n->header, sizeof(struct tar_header));
-		if(strcmp(n->header->typeflag, "0")==0)
-			write(fdd, n->file, strtoul(n->header->size,NULL,8));
+		if(strcmp(n->header->typeflag, "0")==0){
+			int file_size = strtoul(n->header->size, NULL, 8);
+			if(file_size!=0) write(fdd, n->file, 512*((file_size/512)+1));
+		}
 	}
 
 	int i;
